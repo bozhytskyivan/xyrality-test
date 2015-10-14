2ef6c28896f3111bd3252502af09ccd5570e9d85
vandza <bojivan.pro@gmail.com>
2015-10-14 12:36:33 +0300

    First commit. Added ui classes

---
+++ app/src/main/java/com/covani/xyralitytest/ui/GamesFragment.java
@@ -0,0 +1,9 @@
+package com.covani.xyralitytest.ui;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Covani on 14.10.2015.
+ */
+public class GamesFragment extends Fragment {
+}
---
+++ app/src/main/java/com/covani/xyralitytest/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.covani.xyralitytest.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Covani on 14.10.2015.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
